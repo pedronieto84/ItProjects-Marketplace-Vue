5eac3dee45050a1d2418db75c62560fb09a25150
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2021-06-22 21:50:22 +0100

    I create simple card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
     <b-container>
-  <b-row align-v="center">
-    <b-col>1 of 3</b-col>
-    <b-col>2 of 3</b-col>
-    <b-col>3 of 3</b-col>
-  </b-row>
+    <b-row align-v="center">
+      <b-col>
+        <b-card
+        title="This is a card"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+        >
+             <b-card-text>Lorem ipsum dolor sit amet, consectetur adipis </b-card-text>
+             <b-button variant="primary">Apply</b-button>
+        </b-card> 
+      </b-col>
+   
+    </b-row>
 </b-container>
 
 </template>
