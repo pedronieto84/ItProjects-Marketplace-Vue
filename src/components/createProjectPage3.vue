2d6e7be1fdf43aa6797009ab2ef7ab2453cc3ee2
rseokju <rseokju@hotmail.com>
2021-09-14 18:58:53 +0200

    edit y delte

--- src/components/createProjectPage3.vue
+++ src/components/createProjectPage3.vue
@@ -8,25 +8,25 @@
       placeholder="Drag and drop files here..."
       class="pt-5 pb-5 mt-5 h-50 text-left"
     ></b-form-file>
-    
+
    <div>
       <h4 class="text-left mb-5">Published files:</h4>
       <p>File name: </p></div>
-    
+
      <div class="d-flex align-items-center justify-content-end">
     <b-button variant="info">publish</b-button>
     </div>
     <div class="d-flex justify-content-start mt-3" >
            <b-button pill variant="outline-danger" class="mb-5" @click="$emit('go-back')">Back</b-button>
         </div>
-    
+
   </div>
   </b-container>
 </template>
 
 <script>
   export default {
-    data() {  
+    data() {
       return {
         file1: null,
         file2: null
